2204a0547cf1b9d9a0e565f15ef4552f9d70762a
Gadañotti Leandro <leoda6@hotmail.com>
2023-12-27 22:49:41 -0300

    se modifico js

--- js/main.js
+++ js/main.js
@@ -26,18 +26,12 @@ class Inversion {
 // Array para almacenar inversiones
 let inversiones = [];
 
-// Función para preguntar al usuario si desea calcular otra inversión
-function calcularOtraInversion() {
-  let respuesta = prompt("¿Desea calcular otra inversión? (Sí/No)").toLowerCase();
-  return respuesta === 'sí' || respuesta === 'si';
-}
-
-// Bucle para cálculos
+let respuesta = "si";
 do {
   let principalInversion = prompt("¿Qué monto desea invertir? (Mínimo 100,000 pesos)");
   principalInversion = parseFloat(principalInversion);
 
-  // Asegurar que la inversión sea de al menos 100,000 pesos
+  //la inversión sea de al menos 100,000 pesos
   if (principalInversion < 100000) {
     alert("La inversión debe ser de al menos 100,000 pesos. Intente nuevamente.");
     continue;
@@ -50,9 +44,16 @@ do {
 
   console.log(`Ganancias totales después de ${inversion.meses} meses: $${gananciasTotales.toFixed(2)}`);
 
-} while (calcularOtraInversion());
+  respuesta = prompt("¿Desea calcular otra inversión? (Sí/No)").toLowerCase();
+} while (respuesta === "si" || respuesta === "sí");
 
 // Obtener la inversión con mayor ganancia
-const inversionMayorGanancia = inversiones.find(inversion => inversion.calcularGanancias() === Math.max(...inversiones.map(inv => inv.calcularGanancias())));
+if (inversiones.length > 0) {
+  const inversionMayorGanancia = inversiones.reduce((prev, current) =>
+    prev.calcularGanancias() > current.calcularGanancias() ? prev : current
+  );
 
-console.log(`La inversión con mayor ganancia fue de $${inversionMayorGanancia.calcularGanancias().toFixed(2)}`);
+  console.log(`La inversión con mayor ganancia fue de $${inversionMayorGanancia.calcularGanancias().toFixed(2)}`);
+} else {
+  console.log("No se realizaron inversiones.");
+}
